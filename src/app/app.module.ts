55aa9271bb6b5567dcf635d8b644232baeb10654
Jesús Martínez Torrecilla <jesusmartorrecilla@gmail.com>
2020-09-17 13:31:08 +0200

    finished card style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AppComponent } from './app.component';
 import { PokemonTypeListComponent } from './pokemon-type-list/pokemon-type-list.component';
 import { PokemonInfoComponent } from './pokemon-info/pokemon-info.component';
 import { PokemonMainListComponent } from './pokemon-main-list/pokemon-main-list.component';
+import { PokemonCardComponent } from './components/pokemon-card/pokemon-card.component';
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { PokemonMainListComponent } from './pokemon-main-list/pokemon-main-list.
     PokemonTypeListComponent,
     PokemonInfoComponent,
     PokemonMainListComponent,
+    PokemonCardComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/pokemon-main-list/pokemon-main-list.component.ts
+++ src/app/pokemon-main-list/pokemon-main-list.component.ts
@@ -10,6 +10,7 @@ export class PokemonMainListComponent implements OnInit {
   public pokemonsTypeList = {};
   public showpokemonsTypeList = [];
   public isLoadingData = false;
+  public idPokemonSelected = '';
   
   constructor(private pokemonService: PokemonService) {
     this.loadPokemons();
@@ -30,6 +31,7 @@ export class PokemonMainListComponent implements OnInit {
   }
 
   onSelectPokemon(idPokemon) {
+    this.idPokemonSelected = idPokemon;
     this.pokemonService.selectPokemon(idPokemon);
   }
 
